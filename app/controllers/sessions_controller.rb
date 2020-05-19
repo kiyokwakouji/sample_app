779d758ded9028e443e8a33e58dd12bd43c28f78
kiyokawa <your.kohji12k@gmail.com>
2020-05-19 13:30:47 +0900

    Add user following

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+ 
  def new
  end
   
@@ -10,8 +11,8 @@ class SessionsController < ApplicationController
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
     else
-      message  = "Account not activated. "
-      message += "Check your email for the activation link."
+      message  = "アカウントが有効化されていません。 "
+      message += "送信されたメールから、アカウントが有効化されているかを確認してください。"
       flash[:warning] = message
       redirect_to root_url
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
- before_action :correct_user, only: [:edit, :update]
- before_action :admin_user, only: :destroy
+ before_action :logged_in_user, only: [:index, :edit, :update, :destroy, 
+                                       :following, :followers]
+ before_action :correct_user,   only: [:edit, :update]
+ before_action :admin_user,     only: :destroy
  
  def index
-  @users = User.where(activated: true).paginate(page: params[:page])
+  @users = User.paginate(page: params[:page])
  end
 
  def show
@@ -19,7 +20,8 @@ class UsersController < ApplicationController
  def create
   @user = User.new(user_params)   
   if @user.save
-     UserMailer.account_activation(@user).deliver_now
+     #UserMailer.account_activation(@user).deliver_now
+     @user.send_activation_email
      flash[:info] = "Please check your email to activate your account."
      redirect_to root_url
   else
@@ -41,17 +43,31 @@ class UsersController < ApplicationController
    end
  end
 
-def destroy
+ def destroy
    User.find(params[:id]).destroy
    flash[:success] = "User deleted"
    redirect_to users_url
-end
+ end
+
+ def following
+  @title = "Following"
+  @user  = User.find(params[:id])
+  @users = @user.following.paginate(page: params[:page])
+  render 'show_follow'
+ end
+
+ def followers 
+  @title = "Followers"
+  @user  = User.find(params[:id])
+  @users = @user.followers.paginate(page: params[:page])
+  render 'show_follow'
+ end
 
  private
 
  def user_params
   params.require(:user).permit(:name, :email, :password,
-                            :password_confirmation)
+                               :password_confirmation)
  end
 
  #正しいユーザーかどうか確認
@@ -62,6 +78,7 @@ end
 
  #管理者かどうか確認
  def admin_user
-   redirect_to(root_url) unless current_user.admin?
+   #redirect_to(root_url) unless current_user.admin?
+   redirect_to(root_path) unless current_user && current_user.admin?
  end
 end
