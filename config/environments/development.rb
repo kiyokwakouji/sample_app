4216f88c8a38ef92a0b1c3b8db3405d42896c758
kiyokawa <your.kohji12k@gmail.com>
2020-05-20 13:58:32 +0900

    development修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,18 +21,16 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'stormy-hamlet-20622.herokuapp.com'    #'localhost:3000' 自分の環境に合わせる
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  host = 'localhost:3000' #自分の環境に合わせる
+  config.action_mailer.default_url_options = { host: 'localhost' , protocol: '3000' }
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'stormy-hamlet-20622.herokuapp.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
