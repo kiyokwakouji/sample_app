0574ccd00711fc7a6bee776c104010441c364853
kiyokawa <your.kohji12k@gmail.com>
2020-05-21 16:58:40 +0900

    S3_BUKET_name変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,6 +7,6 @@ if Rails.env.production?
         :aws_access_key_id     => ENV['AKIAVS54BXYASLOLVNKT'],                    #S3_ACCESS_KEY
         :aws_secret_access_key => ENV['BtK0yzlhVj/jY8kjn5aOGWQW3c0x5E9DnFPGeiYR'] #S3_SECRET_KEY
       }
-      config.fog_directory     =  ENV['20622-sample.app']                        #S3_BUCKET
+      config.fog_directory     =  ENV['S20622-sample.app']                        #S3_BUCKET
     end
   end
